a15bb5f3bdae310b2005c1dc96f418ef6fa56074
Vaibhav <vaibhavgupta2980702@gmail.com>
2025-04-16 19:41:10 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import { useEffect, useMemo, useState } from 'react';
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect, useMemo, useState } from "react";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import Home from './pages/home/Home';
-import Header from './layout/Header';
-import Footer from './layout/Footer';
-import About from './pages/about/About';
-import Resume from './pages/resume/Resume';
-import Loading from './components/Loading';
-import Project from './pages/project/Project';
-import PageNotFound from './pages/404/PageNotFound';
-import getLPTheme from './components/theme/getLPTheme';
-import Contact from './pages/contact/Contact';
-
+import Home from "./pages/home/Home";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import About from "./pages/about/About";
+import Resume from "./pages/resume/Resume";
+import Loading from "./components/Loading";
+import Project from "./pages/project/Project";
+import PageNotFound from "./pages/404/PageNotFound";
+import getLPTheme from "./components/theme/getLPTheme";
+import Contact from "./pages/contact/Contact";
 
 // App component
 export default function App() {
-
-  const [isLoading, setIsLoading] = useState(true)
-  const [mode, setMode] = useState(localStorage.getItem('mode') ?? 'dark')
+  const [isLoading, setIsLoading] = useState(true);
+  const [mode, setMode] = useState(localStorage.getItem("mode") ?? "dark");
 
   const LPtheme = useMemo(() => createTheme(getLPTheme(mode)), [mode]);
 
@@ -30,40 +28,38 @@ export default function App() {
   }, []);
 
   const handleModeChange = () => {
-    const newMode = mode === 'light' ? 'dark' : 'light';
+    const newMode = mode === "light" ? "dark" : "light";
     setMode(newMode);
-    localStorage.setItem('mode', newMode);
+    localStorage.setItem("mode", newMode);
   };
 
   return (
     <ThemeProvider theme={LPtheme}>
       <CssBaseline />
 
-      {isLoading ? (<Loading />) : (
-
+      {isLoading ? (
+        <Loading />
+      ) : (
         <BrowserRouter>
-
           <Header mode={mode} modeChange={handleModeChange} />
 
-          <div style={{ marginTop: '100px' }}>
+          <div style={{ marginTop: "100px" }}>
             <Routes>
-              <Route path='/home' element={<Home />} />
-              <Route path='/resume' element={<Resume />} />
-              <Route path='/contact' element={<Contact />} />
-              <Route path='/404' element={<PageNotFound />} />
-              <Route path='/about' element={<About mode={mode} />} />
-              <Route path='/project' element={<Project mode={mode} />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/resume" element={<Resume />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/404" element={<PageNotFound />} />
+              <Route path="/about" element={<About mode={mode} />} />
+              <Route path="/project" element={<Project mode={mode} />} />
 
               <Route path="*" element={<Navigate to="/404" />} />
-              <Route path='/' element={<Navigate to='/home' />} />
+              <Route path="/" element={<Navigate to="/home" />} />
             </Routes>
           </div>
 
           <Footer />
-
         </BrowserRouter>
       )}
-
     </ThemeProvider>
-  )
+  );
 }
